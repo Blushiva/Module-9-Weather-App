9bc11f8e7ca99368815df35ed2c372ab07ece45e
Brandi R <167109599+Blushiva@users.noreply.github.com>
2024-09-23 20:51:34 -0500

    Update weatherService.ts
    updated in class the api address

--- server/src/service/weatherService.ts
+++ server/src/service/weatherService.ts
@@ -11,10 +11,42 @@ interface Coordinates {
 
 // TODO: Complete the WeatherService class
 class WeatherService {
+    id: number;
+    main:string;
+    description:string;
+    icon:string;
+
+    constructor(id, main, description, icon){
+        this.id = id;
+        this.main = main;
+        this.description = description;
+        this.icon = icon;
+    }
+    const myWeather = new WeatherService(1, 'Clouds', 'overcast clouds', '04d');
+    console.log(myWeather);
+
+    class weatherService {
+
+
   // TODO: Define the baseURL, API key, and city name properties
-  // TODO: Create fetchLocationData method
-  // private async fetchLocationData(query: string) {}
+   baseURL: string;
+    apiKey: string;
+    cityName: string;
+
+
+  constructor(baseURL: string, apiKey: string, cityName: string) {
+    this.baseURL = baseURL ?? 'https://api.openweathermap.org/data/2.5/weather';
+    this.apiKey = apiKey ??  '3f5ebc304658be2d2264d8e8b1683199';
+    this.cityName = cityName;
+  }
+  // Fetch the location data from the API
   // TODO: Create destructureLocationData method
+
+  private async fetchLocationData(query:string) {
+  //3f5ebc304658be2d2264d8e8b1683199 api key from weather URL
+  const response = await fetch(this.baseURL);
+
+
   // private destructureLocationData(locationData: Coordinates): Coordinates {}
   // TODO: Create buildGeocodeQuery method
   // private buildGeocodeQuery(): string {}
@@ -30,6 +62,12 @@ class WeatherService {
   // private buildForecastArray(currentWeather: Weather, weatherData: any[]) {}
   // TODO: Complete getWeatherForCity method
   // async getWeatherForCity(city: string) {}
+  async getWeatherForCity(city: string) {
+    //https://api.openweathermap.org/data/2.5/weather?q=London&appid=3f5ebc304658be2d2264d8e8b1683199 
+    
+    const URL = '${this.baseURL}?q=${city}&appid=${this.apiKey}';
+    const weatherForCity  = await fetch(URL);
+    console.log(weatherForCity);
 }
 
 export default new WeatherService();
