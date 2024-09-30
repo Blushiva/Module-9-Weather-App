50ec357ddcd232d130fa8463dd9425f2230d0e01
Brandi R <167109599+Blushiva@users.noreply.github.com>
2024-09-30 14:36:06 -0500

    Forgot to update notes
    changed city class after researching it
    
    changed History Service after researching

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -1,14 +1,23 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
+;
+
+import weatherService from '../../service/weatherService.js';
+
 const router = Router();
+const weatherService = new WeatherService('https://api.openweathermap.org/data/2.5/weather', '3f5ebc304658be2d2264d8e8b1683199', 'cityName');
 
 // import HistoryService from '../../service/historyService.js';
- import WeatherService from '../../service/weatherService.js';
+ 
 
 // TODO: POST Request with city name to retrieve weather data
+// error from html routes ts  due to missing imports look further into this
 router.post('/', (req: Request, res: Response) => {
   console.log(req, res);
   
   // TODO: GET weather data from city name
+   const weatherService = new WeatherService();
+    weatherService.getWeatherForCity(req.body.city);
+} );
   // TODO: save city to search history
 });
 
--- server/src/service/historyService.ts
+++ server/src/service/historyService.ts
@@ -1,62 +1,71 @@
 import { get } from "http";
+import * as fs from 'fs';
 
 // TODO: Define a City class with name and id properties
-     class City {
-       id: string;
-       name: string;
-       constructor(id: string, name: string) {
-         this.id = id;
-         this.name = name;
-       }
-     }
+class City {
+  id: string;
+  name: string;
+  constructor(id: string, name: string) {
+    this.id = id;
+    this.name = name;
+  }
+}
 
 // TODO: Complete the HistoryService class
 class HistoryService {
   private cities: City[] = [];
+
   constructor() {
     this.read().then((cities) => {
-      this.cities = cities
-    });}}
+      this.cities = cities;
+    });
+  }
 
-
-    // the logic to read the searchHistory.json file
-    private async read(): Promise<City[]> {
+  // the logic to read the searchHistory.json file
+  private async read(): Promise<City[]> {
+    try {
+      const data = await fs.promises.readFile('searchHistory.json', 'utf-8');
+      return JSON.parse(data);
+    } catch (error) {
+      console.log('Error reading searchHistory.json', error);
       return [];
-
-    public async read(): Promise<City[]> {
-      get cities() {};
-
-      public async addCity(city: City): void {
-        this.cities.push(city);
-        this.write();
-      }
-      public removeCity(id: string): void{
-        this.cities = this.cities.filter(city => city.id !== id);
-        this.write();
-      }
-
-      export default HistoryService;
-
-  // TODO: Define a read method that reads from the searchHistory.json file
-     private async read() {
-        const data = await fs.promises.readFile('searchHistory.json', 'utf-8');
-        return JSON.parse(data);
-     }
     }
+  }
+  // TODO Define an addCity method that adds a city to the searchHistory.json file
+  // the logic to add a city to the searchHistory.json file
+  public async addCity(city: City): Promise<void> {
+    this.cities.push(city);
+    await this.write();
+  }
+  // TODO: Define a getCities method that reads the cities from the searchHistory.json file and returns them as an array of City objects
+
+
+  
 
   // TODO: Define a write method that writes the updated cities array to the searchHistory.json file
-  // private async write(cities: City[]) {}
+  private async write(): Promise<void> {
+    try {
+      await fs.promises.writeFile('searchHistory.json', JSON.stringify(this.cities));
+    } catch (error) {
+      console.log('Error writing to searchHistory.json', error);
+    }
+  }
+
+  // * BONUS TODO: Define a removeCity method that removes a city from the searchHistory.json file
+  private removeCity(id: string): void {
+    this.cities = this.cities.filter(city => city.id !== id);
+    await this.write();
+  }
+  // async removeCity(id: string) {}
+
+}
 
 
   // TODO: Define a getCities method that reads the cities from the searchHistory.json file and returns them as an array of City objects
-  // async getCities() {}
-
-
-  // TODO Define an addCity method that adds a city to the searchHistory.json file
-  // async addCity(city: string) {}
-
-  // * BONUS TODO: Define a removeCity method that removes a city from the searchHistory.json file
-  // async removeCity(id: string) {}
+  private async getCities(): Promise<City[]> {
+    return this.read();
+  }
 }
+
 
 export default new HistoryService();
