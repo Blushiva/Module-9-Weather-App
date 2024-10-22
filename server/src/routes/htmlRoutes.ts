f9082d3db7bba65bffab1810179fb9a46b57fe4b
Brandi R <167109599+Blushiva@users.noreply.github.com>
2024-10-22 14:56:54 -0500

    added additional routing and tried to update the weatherservice class

--- server/src/routes/htmlRoutes.ts
+++ server/src/routes/htmlRoutes.ts
@@ -9,28 +9,18 @@ import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { Router, Request, Response } from 'express';
 import { readFile } from 'fs';
+// imports from weatherservice
 import WeatherService from '../service/weatherService';
+
+// handles file paths in modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+// Creates instance of wetherService class
 const router = Router();
-const weatherService = new WeatherService('https://api.openweathermap.org/data/2.5/weather', '3f5ebc304658be2d2264d8e8b1683199', '');
+const weatherService = new weatherService('https://api.openweathermap.org/data/2.5/weather', '3f5ebc304658be2d2264d8e8b1683199', '');
 
-router.post('/', async (req: any, res: any) => {
-  try {
-    const { city } = req.body;
-    if (!city) {
-      return res.status(400).send('City is required');
-    }
-
-    await weatherService.getWeatherForCity(city);
-    res.status(200).send('Weather data fetched successfully');
-  } catch (error) {
-    console.error('Error fetching weather data:', error);
-    res.status(500).send('Internal Server Error');
-  }
-});
-
-// TODO: Define route to serve index.html
+// Defines route to serve index.html
 router.get('/', (req:Request, res:Response) => {
   res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
   const fullPath = path.join(__dirname, '../../client/dist/index.html');
@@ -43,4 +33,24 @@ router.get('/', (req:Request, res:Response) => {
     });
   });
 
+  //defines the route to index html file
+router.post('/', async (req: any, res: any) => {
+  try {
+    const { city } = req.body;
+    if (!city) {
+      return res.status(400).send('City is required');
+    }
+//gets the weather data for the city
+    const weatherData = await weatherService.getWeatherForCity(city);
+
+    //sends the weather data as a response
+    res.status(200).send('Weather data fetched successfully');
+  } catch (error) {
+    console.error('Error fetching weather data:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+
+//exports the router as the default export
 export default router;
